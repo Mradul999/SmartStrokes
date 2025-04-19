b1d8ebc434ee7d43a80d7adf8b4ab7616e49b496
prashantkumar999 <prashant047alg@gmail.com>
2025-04-19 16:47:47 +0530

    user and user-typing-session model added

---
+++ backend/models/UserTypingSession.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userTypingSessionSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    textGiven: {
+        type: String,
+        required: true
+    },
+    textTyped: {
+        type: String,
+        required: true
+    },
+    correctChars: {
+        type: Number,
+        default: 0
+    },
+    incorrectChars: {
+        type: Number,
+        default: 0
+    },
+    wpm: {
+        type: Number,
+        required: true
+    },
+    accuracy: {
+        type: Number,
+        required: true
+    },
+    weakKeys: {
+        type: [String],
+        default: []
+    },
+});
+
+module.exports = mongoose.model('UserTypingSession', userTypingSessionSchema);
