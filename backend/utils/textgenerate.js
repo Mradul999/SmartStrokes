9e204f0b62743fda269744ad5a6a99cc08e0bedb
Mradul999 <demorgans007@gmail.com>
2025-05-20 14:33:45 +0530

    commit

--- backend/utils/textgenerate.js
+++ backend/utils/textgenerate.js
@@ -7,7 +7,7 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 export const textGenerate = async (req, res) => {
   const wrongKeyPresses = req.body.misTypes;
 
-  const prompt = `generate a 200 word grammatically correct passage in english using only lowercase letters and no punctuation marks make the tone engaging and interesting additionally use the following letters  from the array (${wrongKeyPresses}).(If this array is empty ignore)  more frequently throughout the passage to help the user practice and improve their typing accuracy for these specific keys`;
+  const prompt = `generate a 200 word grammatically correct passage in english using both lower case and upper case with punctuations like full stop ,commas  marks make the tone engaging and interesting additionally use the following letters  from the array (${wrongKeyPresses}).(If this array is empty ignore)  more frequently throughout the passage to help the user practice and improve their typing accuracy for these specific keys`;
 
   try {
     const response = await ai.models.generateContent({
