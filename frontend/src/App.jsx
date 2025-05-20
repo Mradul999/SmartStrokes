4017f7000f35964f3ab1938c15c6b04cf5ff900f
Mradul999 <demorgans007@gmail.com>
2025-05-20 11:49:38 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
@@ -16,16 +17,37 @@ import { ThemeProvider } from "./context/ThemeContext";
 import Test from "./pages/Test";
 
 function App() {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1024);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 1024);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (!isDesktop) {
+    return (
+      <div className="h-screen flex items-center justify-center text-center px-4">
+        <h1 className="text-xl font-semibold">
+          Website is only available for desktop devices.
+        </h1>
+      </div>
+    );
+  }
+
   return (
     <ThemeProvider>
       <BrowserRouter>
         <Layout>
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/contact" element={<Contact />}></Route>
-            <Route path="/privacy" element={<Privacy />}></Route>
-            <Route path="/terms" element={<Terms />}></Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/terms" element={<Terms />} />
             <Route
               path="/signup"
               element={
@@ -43,10 +65,10 @@ function App() {
               }
             />
             <Route path="/otp-verification" element={<OTPVerification />} />
-            <Route path="/dashboard" element={<Dashboard />}></Route>
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/reset-password/:token" element={<ResetPassword />} />
-            <Route path="/test" elemt={<Test />}></Route>
+            <Route path="/test" element={<Test />} />
           </Routes>
         </Layout>
       </BrowserRouter>
